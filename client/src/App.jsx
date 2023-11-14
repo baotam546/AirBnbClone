b785358c9b1d027e398d9adc9ce3111cda1d1aa5
baotam546 <donguyenbaotam546@gmail.com>
2023-11-14 20:49:46 +0700

    client side first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import {Routes, Route} from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+
+
+  return (
+    <>
+    <Routes>
+      <Route index element={<IndexPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+    </Routes>
+      
+    </>
+  )
+}
+
+export default App
